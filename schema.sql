9e32e0fadfdfc1deeb3073cf89568d72554a4779
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-07-12 17:13:21 +0300

    create mysql schema

---
+++ schema.sql
@@ -0,0 +1,8 @@
+create table if not exists users(
+  id varchar(36) NOT NULL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  avatar varchar(255) NOT NULL,
+  UNIQUE KEY email (email)
+)
--- user_handler.go
+++ user_handler.go
@@ -36,5 +36,4 @@ func UserRegister(w http.ResponseWriter, r *http.Request) {
 
 	render.Status(r, http.StatusOK)
 	render.JSON(w, r, user)
-
 }
