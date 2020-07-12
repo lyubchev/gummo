45cfacbbfda7da6018fdac56518233ad7d083351
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-07-12 19:06:07 +0300

    add middlewares

--- web.go
+++ web.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-redis/redis"
 	"upper.io/db.v3"
 )
@@ -18,9 +20,18 @@ func NewWeb(db db.Database, rdb *redis.Client) *Web {
 	r := chi.NewRouter()
 	wb := &Web{Router: r, DB: db, Redis: rdb}
 
+	r.Use(
+		middleware.Logger,
+		middleware.Recoverer,
+		middleware.Heartbeat("/ping"),
+		middleware.RedirectSlashes,
+		middleware.StripSlashes,
+		middleware.Timeout(time.Minute),
+	)
+
 	r.Route("/user", func(r chi.Router) {
 		r.Post("/register", wb.UserRegister)
-		// r.Post("/login")
+		r.Post("/login", wb.UserLogin)
 	})
 
 	return wb
